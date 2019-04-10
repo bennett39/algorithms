c8f1be9eaf5acf362c315a7e04b1bf27d7a7eb63
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-10 16:48:50 -0400

    Implement quicksort

---
+++ quicksort.py
@@ -0,0 +1,19 @@
+def quicksort(unsorted, lo, hi):
+    if hi - lo < 2:
+        return unsorted
+    pivot = unsorted[lo]
+    bumper = lo
+    for i in range(lo, hi):
+        if unsorted[i] < pivot:
+            bumper += 1
+            unsorted[i], unsorted[bumper] = unsorted[bumper], unsorted[i]
+    unsorted[lo], unsorted[bumper] = unsorted[bumper], unsorted[lo]
+    quicksort(unsorted, lo, bumper-1)
+    quicksort(unsorted, bumper+1, hi)
+    return unsorted
+
+
+unsorted = [5, 9, 2, 3, 8, 3, 1]
+test = [1, 2, 3, 3, 5, 8, 9]
+
+assert quicksort(unsorted, 0, len(unsorted)) == test
