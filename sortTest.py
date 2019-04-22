b74916aff1791407d8a77d38ddcf07d5a2f45c3f
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-22 09:37:50 -0400

    Make speed test more robust w/ random list and wrapper timer function

--- sortTest.py
+++ sortTest.py
@@ -1,30 +1,38 @@
+import random
 from timeit import default_timer
 from quicksort import quicksort
 from mergesort import mergesort
 
-test_data = [
-        5, 2, 4, 6, 9, 1, 4, 2, 7, 9, 1,
-        2, 9, 3, 1, 7, 4, 2, 8, 7, 3, 5,
-        6, 2, 7, 3, 1, 1, 6, 9, 3, 5, 8,
-        2, 7, 4, 1, 3, 9, 8, 5, 3, 5, 7,
-        2, 8, 4, 6, 9, 1, 4, 5, 3, 4, 6
-]
+def main():
+    test_data = generate_random_list(10000)
+    assert sorted_timed(test_data) == quicksort_timed(test_data) \
+            == mergesort_timed(test_data)
 
-standard_start = default_timer()
-s = sorted(test_data)
-standard_end = default_timer()
+def timer(f):
+    def wrapper(*args, **kwargs):
+        begin = default_timer()
+        result = f(*args, **kwargs)
+        end = default_timer()
+        print(f"Function took {end - begin} seconds to compute.")
+        return result
+    return wrapper
+
+@timer
+def sorted_timed(*args, **kwargs):
+    return sorted(*args, **kwargs)
+
+@timer
+def quicksort_timed(test_data, *args, **kwargs):
+    n = len(test_data)
+    return quicksort(test_data, 0, n)
+
+@timer
+def mergesort_timed(*args, **kwargs):
+    return mergesort(*args, **kwargs)
+
+def generate_random_list(length):
+    return [random.randint(-100, 100) for _ in range(length)]
 
 
-quick_start = default_timer()
-n = len(test_data)
-q = quicksort(test_data, 0, n)
-quick_end = default_timer()
-
-merge_start = default_timer()
-m = mergesort(test_data)
-merge_end = default_timer()
-
-assert q == m == s
-print(f"Standard: {standard_end - standard_start}")
-print(f"Quicksort: {quick_end - quick_start}")
-print(f"Mergesort: {merge_end - merge_start}")
+if __name__ == "__main__":
+    main()
