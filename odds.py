43843012fa2835971017e2621b1b6d5e5cda3c89
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-05-09 12:07:34 -0400

    Make range inclusive (+1)

--- odds.py
+++ odds.py
@@ -1,8 +1,9 @@
 def odds(m, n):
-    """ Prints all odd numbers in range m - n. m is optional """
+    """ Prints all odd numbers in range m - n. """
     if not m & 1:
         m += 1
-    for i in range(m, n, 2):
+    for i in range(m, n+1, 2):
         print(i)
 
 odds(0, 10)
+odds(20, 35)
